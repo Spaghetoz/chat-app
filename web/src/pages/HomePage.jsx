e481cc5ea9a9d89e2e15c1557713a74b8d979ead
Spaghetoz <80428139+Spaghetoz@users.noreply.github.com>
2025-09-16 10:25:00 +0200

    add home page

--- web/src/pages/HomePage.jsx
+++ web/src/pages/HomePage.jsx
@@ -1,10 +1,35 @@
-
+import { NavLink } from "react-router";
 
 export default function HomePage() {
 
   return(
-    <div>
-      
+    <div className="">
+      <section class="bg-neutral-900 lg:grid lg:h-screen lg:place-content-center dark:bg-gray-900">
+        <div class="mx-auto w-screen max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8 lg:py-32">
+          <div class="max-w-prose">
+            <h1 class="text-4xl font-bold text-neutral-200 sm:text-5xl dark:text-white">
+              Chat & Whiteboard
+              <strong class="text-blue-600"> in one place </strong>
+            </h1>
+
+            <p class="mt-4 text-base text-pretty text-neutral-400 sm:text-lg/relaxed dark:text-neutral-200">
+              Prototype app to chat and collaborate visually with a live whiteboard. 
+              Perfect to test ideas, draw, and communicate in real time.
+            </p> {/**TODO change text */}
+
+            <div class="mt-4 flex gap-4 sm:mt-6">
+              
+              <NavLink to="/" end>
+                <a
+                  class="inline-block rounded border border-blue-600 bg-blue-600 px-5 py-3 font-semibold text-neutral-100 shadow-sm transition-colors hover:bg-blue-700"
+                >
+                  Start chatting
+                </a>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
